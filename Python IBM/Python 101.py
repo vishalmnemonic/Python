06df7b725c406625c9aef2e3117aa3297dcd4bec
Vishal Kumar <vishalks1993@gmail.com>
2018-05-09 23:07:46 +0530

    Learning

---
+++ Python IBM/Python 101.py
@@ -0,0 +1,27 @@
+print("Hello World")
+
+# Comment
+
+type(11)
+type(11.2)
+type("Hi")
+
+int(1.2):1
+float(1):1.0
+int(True):1
+bool(1):True
+
+25/5:5.0
+26//5:5
+
+my_variable = 1
+my_variable
+
+x = 1 + 2 + 3
+y = x + 5
+y = y + 6
+
+my = "Vishal"
+my[0]:V
+my[-1]:l
+my[0:2]:Vi
