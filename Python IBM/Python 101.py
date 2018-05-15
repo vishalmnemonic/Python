6f08f4b6f976c52b2adb665295433e59453bc974
Vishal Kumar <vishalks1993@gmail.com>
2018-05-15 09:35:00 +0530

    Update Python 101.py

--- Python IBM/Python 101.py
+++ Python IBM/Python 101.py
@@ -2,7 +2,7 @@ print("Hello World")
 
 # Comment
 
-type(11)
+type(11)	Represents different types of data: int, float, str
 type(11.2)
 type("Hi")
 
@@ -25,3 +25,22 @@ my = "Vishal"
 my[0]:V
 my[-1]:l
 my[0:2]:Vi
+my[::2]:"Vsa"
+my[start:stop-1:gap]
+
+len("Vishal")
+concatenate using + symbol
+my + "is my name"
+3 * my
+Strings are immutable, but can create a new string
+\n represents a new line
+\t represents a tab
+\\ represents a \
+
+Method:
+A="I am the best"
+B=A.replace('I am', 'She is')
+
+A.find('am'):3
+A.find('z'):-1
+
