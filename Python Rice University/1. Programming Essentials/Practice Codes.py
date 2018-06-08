84f2099b8d6193db48da3f9e4bfd5be7eff958c7
Vishal Kumar <vishalks1993@gmail.com>
2018-06-08 11:33:35 +0530

    Rice Uni. Week 1

---
+++ Python Rice University/1. Programming Essentials/Practice Codes.py
@@ -0,0 +1,15 @@
+Python from Rice
+
+print("Hello World")
+
+"""Document String: Docstring"""
+# Comments
+
+int, float, 
+1e500:infinity
+
+print(1+3.2)
+/: float division
+//: Int division
+print(3**2): exponent
+
