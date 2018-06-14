14c4ec7e783685db11cc339be4f388303b917ea7
Vishal Kumar <vishalks1993@gmail.com>
2018-06-14 22:40:20 +0530

    Michigan Python

---
+++ Python Michigan University/1. Programming for Everybody/Practice Codes.py
@@ -0,0 +1,17 @@
+Python from Michigan
+
+Pattern of code:
+Sequence
+Conditional
+Repeated
+
+Constant and Reserved words
+
+Choose variable names that make sense
+Int, Float, String
+type("abc"):String
+type(1):Int
+float(10)
+
+name=input("Who are you")
+print('Welcome', name)
